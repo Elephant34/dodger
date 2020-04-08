05642dedafcf8727f3d58a2616cc75cbee83c786
Elephant34 <51174423+Elephant34@users.noreply.github.com>
2020-04-08 07:12:55 +0100

    Create dodger.py

---
+++ dodger.py
@@ -0,0 +1,29 @@
+'''
+A game- player dodge the falling blocks
+'''
+import pyglet
+
+class Window(pyglet.window.Window):
+    '''
+    The window class with custom draw function
+    '''
+
+    def __init__(self, width, height, **kawgs):
+        '''
+        Sets up the main window
+        '''
+        super().__init__(width, height, **kawgs)
+    
+    def on_draw(self):
+        '''
+        Overwrites the main draw function
+        '''
+        self.clear()
+
+        # Draws all the other needed items
+
+window = Window(600, 800)
+window.set_caption("Dodger")
+
+if __name__ == "__main__":
+    pyglet.app.run()
